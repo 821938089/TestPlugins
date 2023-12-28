3a641f5d1c2eab63a013778a00919bc54edb44a4
KillerDogeEmpire <edison.beckett@gmail.com>
2023-12-27 22:50:57 -0800

    Test fix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,6 +78,8 @@ subprojects {
         implementation("org.jsoup:jsoup:1.16.2") // html parser
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.0")
         implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
+        implementation("io.karn:khttp-android:0.1.2")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     }
 }
 
