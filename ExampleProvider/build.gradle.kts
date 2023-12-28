2b1e7a2eac73da349628271a1d48994966a18a8a
KillerDogeEmpire <edison.beckett@gmail.com>
2023-12-27 22:36:39 -0800

    Tedt

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,38 +1,26 @@
-dependencies {
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.recyclerview:recyclerview:1.2.1")
-}
 // use an integer for version numbers
-version = -1
+version = 35
 
 
 cloudstream {
+    language = "en"
     // All of these properties are optional, you can safely remove them
 
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst")
+    description = "Watch Aniwave/9anime, also check out Enimax's app/chrome extension, He was a big help for fixing the extension, here is the link https://github.com/enimax-anime/enimax"
+    authors = listOf("Stormunblessed, KillerDogeEmpire, Enimax, Chokerman")
 
     /**
-    * Status int as the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta only
-    * */
-    status = 1
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "Anime",
+        "OVA",
+    )
 
-    tvTypes = listOf("NSFW")
-
-    requiresResources = true
-    language = "en"
-
-    // random cc logo i found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
-}
-
-android {
-    buildFeatures {
-        viewBinding = true
-    }
+    iconUrl = "https://www.google.com/s2/favicons?domain=aniwave.to&sz=%size%"
 }
